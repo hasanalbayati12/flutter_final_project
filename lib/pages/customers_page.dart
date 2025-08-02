f8631a2f54308519b137496d9895f6dee4717c2d
HasanAlbayati12 <Hassan.albayati12@gmail.comgit config --global user.name HasanAlbayati12 git config --global user.email Hassan.albayati12@gmail.com>
2025-08-02 14:24:36 -0400

    added form controllers to the customer page

--- lib/pages/customers_page.dart
+++ lib/pages/customers_page.dart
@@ -13,3 +13,37 @@ class CustomersPage extends StatefulWidget {
   @override
   State<CustomersPage> createState() => _CustomersPageState();
 }
+/// State class for CustomersPage.
+/// Manages customer data, form controllers, and UI state.
+class _CustomersPageState extends State<CustomersPage> {
+  final CustomerRepository _repository = CustomerRepository();
+  final EncryptedSharedPreferences _prefs = EncryptedSharedPreferences();
+
+  List<Customer> _customers = [];
+  Customer? _selectedCustomer;
+  bool _isLoading = true;
+  bool _isAddingNew = false;
+
+  // Form controllers for customer input fields
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _dateOfBirthController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCustomers();
+  }
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _addressController.dispose();
+    _dateOfBirthController.dispose();
+    super.dispose();
+  }
+  /// Closes the details view and returns to the customer list.
+  void _closeDetails() {
+    _clearForm();
+  }
